8e78169893009ffc1df5318d569efd218d06c5c1
miahmo652 <miahmo652@users.noreply.github.com>
2020-03-13 20:49:07 -0400

    update

--- index.js
+++ index.js
@@ -13,9 +13,9 @@
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+    return callback(stringList[0])
 }
 
 // ⭐️ Example Challenge END ⭐️
@@ -47,9 +47,11 @@ function processFirstItem(stringList, callback) {
  * 
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
-*/
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function processLength(list, callback) {
+    let process = callback(list.length)
+
+    return process;
 }
 
 /**
@@ -65,9 +67,12 @@ function processLength(/* CODE HERE */) {
  * Example of usage of this higher-order function:
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
-*/
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function processLastItem(stringList, callback) {
+    return (callback(stringList.pop()))
+
+
+
 }
 
 /**
@@ -87,9 +92,9 @@ function processLastItem(/* CODE HERE */) {
  * 
  * [2] Invoking `processSum` passing `-5`, '-1', and `(num) => num + 1000`,
  * should return 994.
-*/
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function processSum(num1, num2, callback) {
+    return callback(num1 + num2);
 }
 
 /**
@@ -109,9 +114,9 @@ function processSum(/* CODE HERE */) {
  * 
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
-*/
-function processProduct(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function processProduct(num1, num2, callback) {
+    return callback(num1 * num2);
 }
 
 /**
@@ -131,9 +136,9 @@ function processProduct(/* CODE HERE */) {
  * 
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
-*/
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+ */
+function processDuplicateFree( /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */ ) {
+    /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
@@ -154,9 +159,14 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * [1] Invoking `lowerCaseStrings` with `[ 'Orange', 'APPLE', 'banana', 'mAnGo']` will return `[ 'orange', 'apple', 'banana', 'mango' ]`.
  * 
  * [2] Invoking `lowerCaseStrings` with `['a', 'b', 'c' ]` will return `[ 'a', 'b', 'c' ]`.
-*/
-function lowerCaseStrings(/* code here */) {
-  /* code here */
+ */
+function lowerCaseStrings(strings) {
+
+    let items = [];
+    strings.forEach((arrItem) => {
+        items.push(arrItem.toLowerCase())
+    })
+    return items;
 }
 
 /**
@@ -173,9 +183,16 @@ function lowerCaseStrings(/* code here */) {
  * [1] Invoking `isItAnApple` with `[ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ]` will return `[ false, true, false, false, true, false ]`.
  * 
  * [2] Invoking `isItAnApple` with `['a', 'b', 'c' ]` will return `[ false, false, false ]`.
-*/
-function isItAnApple(/* code here */) {
-  /* code here */
+ */
+function isItAnApple(strings) {
+    const apples = strings.map((arrItem) => {
+        if (arrItem === "apple") {
+            return true;
+        } else {
+            return false;
+        }
+    })
+    return apples;
 }
 
 /**
@@ -193,9 +210,9 @@ function isItAnApple(/* code here */) {
  * [1] Invoking `removeApple` with `[ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ]` will return `[ 'orange', 'banana', 'apples', 'mango' ]`.
  * 
  * [2] Invoking `removeApple` with `['a', 'b', 'c' ]` will return `[ 'a', 'b', 'c' ]`.
-*/
-function removeApple(/* code here */) {
-  /* code here */
+ */
+function removeApple(strings) {
+    let apple = strings.filter((arritem))
 }
 
 /**
@@ -212,9 +229,9 @@ function removeApple(/* code here */) {
  * [1] Invoking `stringSmash` with `[ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ]` will return 'orangeapplebananaapplesapplemango'.
  * 
  * [2] Invoking `stringSmash` with `['a', 'b', 'c' ]` will return `abc`.
-*/
-function stringSmash(/* code here */) {
-  /* code here */
+ */
+function stringSmash( /* code here */ ) {
+    /* code here */
 }
 
 // A local community center is holding a fund raising 5k fun run and has invited
@@ -231,9 +248,9 @@ function stringSmash(/* code here */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
-*/
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function getFullNames( /* CODE HERE */ ) {
+    /* CODE HERE */
 }
 
 /**
@@ -247,9 +264,9 @@ function getFullNames(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
-*/
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function firstNamesAllCaps( /* CODE HERE */ ) {
+    /* CODE HERE */
 }
 
 /**
@@ -265,9 +282,9 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @param tShirtSize string (possible values are "S", "M", "L", "XL", "2XL", "3XL").
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
-*/
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function getRunnersByTShirtSize( /* CODE HERE */ ) {
+    /* CODE HERE */
 }
 
 /**
@@ -280,9 +297,9 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * 
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
-*/
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function tallyUpDonations( /* CODE HERE */ ) {
+    /* CODE HERE */
 }
 
 /////////////// CLOSURES ///////////////
@@ -300,14 +317,14 @@ function tallyUpDonations(/* CODE HERE */) {
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ */
 
 // counter1 code
 function counterMaker() {
-  let count = 0;
-  return function counter() {
-    count++;
-  }
+    let count = 0;
+    return function counter() {
+        count++;
+    }
 }
 
 const counter1 = counterMaker();
@@ -316,7 +333,7 @@ const counter1 = counterMaker();
 let count = 0;
 
 function counter2() {
-  return count++;
+    return count++;
 }
 
 /**
@@ -339,9 +356,9 @@ function counter2() {
  * counter() // should return 3
  * counter() // should return 0
  * etc
-*/
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function counterMakerWithLimit( /* CODE HERE */ ) {
+    /* CODE HERE */
 }
 
 /////////////// END OF CHALLENGE ///////////////
@@ -349,22 +366,22 @@ function counterMakerWithLimit(/* CODE HERE */) {
 /////////////// END OF CHALLENGE ///////////////
 
 if (typeof exports !== 'undefined') {
-  // IGNORE: Test/Env Detected
-  // For Node/Non-browser test env
-  module.exports = module.exports || {}
-  if (processFirstItem) { module.exports.processFirstItem = processFirstItem }
-  if (processLength) { module.exports.processLength = processLength }
-  if (processLastItem) { module.exports.processLastItem = processLastItem }
-  if (processSum) { module.exports.processSum = processSum }
-  if (processProduct) { module.exports.processProduct = processProduct }
-  if (processDuplicateFree) { module.exports.processDuplicateFree = processDuplicateFree }
-  if (lowerCaseStrings ) { module.exports.lowerCaseStrings = lowerCaseStrings}
-  if (isItAnApple) { module.exports.isItAnApple = isItAnApple }
-  if (removeApple) { module.exports.removeApple = removeApple }
-  if (stringSmash) { module.exports.stringSmash = stringSmash }
-  if (getFullNames) { module.exports.getFullNames = getFullNames }
-  if (firstNamesAllCaps) { module.exports.firstNamesAllCaps = firstNamesAllCaps }
-  if (getRunnersByTShirtSize) { module.exports.getRunnersByTShirtSize = getRunnersByTShirtSize }
-  if (tallyUpDonations) { module.exports.tallyUpDonations = tallyUpDonations }
-  if (counterMakerWithLimit) { module.exports.counterMakerWithLimit = counterMakerWithLimit }
+    // IGNORE: Test/Env Detected
+    // For Node/Non-browser test env
+    module.exports = module.exports || {}
+    if (processFirstItem) { module.exports.processFirstItem = processFirstItem }
+    if (processLength) { module.exports.processLength = processLength }
+    if (processLastItem) { module.exports.processLastItem = processLastItem }
+    if (processSum) { module.exports.processSum = processSum }
+    if (processProduct) { module.exports.processProduct = processProduct }
+    if (processDuplicateFree) { module.exports.processDuplicateFree = processDuplicateFree }
+    if (lowerCaseStrings) { module.exports.lowerCaseStrings = lowerCaseStrings }
+    if (isItAnApple) { module.exports.isItAnApple = isItAnApple }
+    if (removeApple) { module.exports.removeApple = removeApple }
+    if (stringSmash) { module.exports.stringSmash = stringSmash }
+    if (getFullNames) { module.exports.getFullNames = getFullNames }
+    if (firstNamesAllCaps) { module.exports.firstNamesAllCaps = firstNamesAllCaps }
+    if (getRunnersByTShirtSize) { module.exports.getRunnersByTShirtSize = getRunnersByTShirtSize }
+    if (tallyUpDonations) { module.exports.tallyUpDonations = tallyUpDonations }
+    if (counterMakerWithLimit) { module.exports.counterMakerWithLimit = counterMakerWithLimit }
 }
